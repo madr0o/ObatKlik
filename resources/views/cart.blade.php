60513e4c3d0f015ecbe6d23ab7cc11c0be1a28cb
Dzaky Ahmad Rofiq <202fzdzaky@gmail.com>
2024-12-06 14:19:04 +0700

    update cart.blade

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,14 +1,14 @@
 @extends('welcome')
 @section('content')
-    <div class="container" style="background-color: wheat;">
+    <div class="container">
         <div class="col">
-            <div class="justify-content-start border border-black">
+            <div class="justify-content-start">
                 <h3>Keranjang</h3>
             </div>
             <div class="row">
-                <div class="col border border-black py-2" id="card-item-cart" width="639px" height="126px">
-                    <div class="row" width="579px" height="84px">
-                        <div class="col border border-black align-items-center" style="width: 83px;height:83px;">
+                <div class="col border border-black ps-2 pe-5" id="card-item-cart" width="639px" height="fit-content">
+                    <div class="row" width="579px">
+                        <div class="col" style="width: 83px;">
                             <img class="p-0" src="{{asset('images/item.svg')}}" alt="item-img" style="width: 83px;height:83px;">
                         </div>
                         <div class="col" id="desc-card" style="">
@@ -26,18 +26,14 @@
                         </div>
                     </div>
                 </div>
-                <div class="col border border-black" id="card-transaction">
-                    <div class="row">
-                        <div class="col border border-black py-2" id="card-item-cart"height="126px">
-                            <div class="row"height="84px">
-                                <p><b>Ringkasan belanja</b></p>
-                                <divr class="row d-flex justify-content-between">
-                                    <p class="col d-flex">Total</p>
-                                    <p class="col d-flex justify-content-end"><b>Rp 255.000</b></p>
-                                </div>
-                                <button type="button" class="btn btn-outline-primary ">Beli</button>
-                            </div>
+                <div class="col d-flex" id="card-transaction">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">Ringkasan belanja</h5>
+                          <p class="card-text"><b>Total</b><br> Rp 255.000</p>
+                          <a href="#" class="btn btn-primary">Beli</a>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
