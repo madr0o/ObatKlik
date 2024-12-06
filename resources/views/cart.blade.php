1eca39ae2c7ec1ea7a4f3cfde6f83452a48149a5
Dzaky Ahmad Rofiq <202fzdzaky@gmail.com>
2024-12-06 20:58:16 +0700

    update cart view

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,42 +1,6 @@
 @extends('welcome')
 @section('content')
     <div class="container">
-        {{-- <div class="col">
-            <div class="justify-content-start">
-                <h3>Keranjang</h3>
-            </div>
-            <div class="row">
-                <div class="col border border-black ps-2 pe-5" id="card-item-cart" width="639px" height="fit-content">
-                    <div class="row" width="579px">
-                        <div class="col" style="width: 83px;">
-                            <img class="p-0" src="{{asset('images/item.svg')}}" alt="item-img" style="width: 83px;height:83px;">
-                        </div>
-                        <div class="col" id="desc-card" style="">
-                            <p class="m-0"><b>Neurobion 10 Tablet</b></p>
-                            <p>Per Botol</p>
-                        </div>
-                        <div class="col d-flex flex-column align-items-end" id="prize-&-qty" style="">
-                            <h6><b>Rp 29.900</b></h6>
-                            <div class="d-flex" style="width: 99px;">
-                                <img class="col p-0" src="{{asset('images/icon/trash.svg')}}" alt="trasch-icn" height="21px">
-                                <div class="col p-0 border border-black" id="btn-qty" style="height: fit-content" width="70px">
-                                    test
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex" id="card-transaction">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                          <h5 class="card-title">Ringkasan belanja</h5>
-                          <p class="card-text"><b>Total</b><br> Rp 255.000</p>
-                          <a href="#" class="btn btn-primary">Beli</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
         <div class="col">
             <div>
                 <h3><b>Keranjang</b></h3>
@@ -56,20 +20,23 @@
                                     </div>
                                     <div class="col">
                                         <div class="row">
-                                            <p><b>Rp 175.800</b></p>
+                                            <p class="text-end"><b>Rp 175.800</b></p>
                                         </div>
-                                        <div class="row">
-                                            <div class="col">
-                                                <img src="{{asset('images/icon/trash.svg')}}" alt="">
-                                            </div>
-                                            <div class="card col">
-                                                <div class="row">
-                                                    <a class="col" href="#" style="text-decoration: none; color:black;"><b>-</b></a>
-                                                    <p class="col">1</p>
-                                                    <a class="col" href="#" style="text-decoration: none; color:black;"><b>+</b></a>
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col d-flex justify-content-center align-items-center">
+                                                    <img src="{{asset('images/icon/trash.svg')}}" alt="">
+                                                </div>
+                                                <div class="card col d-flex justify-content-center" style="width: 20px;">
+                                                    <div class="row align-items-center text-center">
+                                                        <a class="col" href="#" style="text-decoration: none; color:black;"><b>-</b></a>
+                                                        <p class="col text-center">1</p>
+                                                        <a class="col" href="#" style="text-decoration: none; color:black;"><b>+</b></a>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
+                                        
                                     </div>
                                 </div>
                             </div>
